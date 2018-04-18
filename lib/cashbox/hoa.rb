098a778deb7b15a5d35a457237dbaf00d29a3017
Maulin Pathare <mpathare@gmail.com>
2018-04-18 17:21:21 -0400

    Allow Cashbox::Hoa to be configured
    LGTMs
      Jon
      Daniel
    
    Closes #5

--- lib/cashbox/hoa.rb
+++ lib/cashbox/hoa.rb
@@ -4,6 +4,14 @@ module Cashbox
   module Hoa
     extend ActiveSupport::Autoload
 
+    class << self
+      attr_accessor :private_key, :auth_id, :javascript_path
+
+      def config(&block)
+        block.call(self)
+      end
+    end
+
     eager_autoload do
       autoload :VERSION
       autoload :Web
--- spec/cashbox/hoa_spec.rb
+++ spec/cashbox/hoa_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe Cashbox::Hoa do
   it "has a version number" do
     expect(Cashbox::Hoa::VERSION).not_to be nil
   end
+
+  describe ".config" do
+    it "allows setting the private_key, auth_id and javascript_path" do
+      Cashbox::Hoa.config do |config|
+        config.private_key = "sekret"
+        config.auth_id = "secret"
+        config.javascript_path = "vindicia.js"
+      end
+
+      expect(Cashbox::Hoa.private_key).to eq("sekret")
+      expect(Cashbox::Hoa.auth_id).to eq("secret")
+      expect(Cashbox::Hoa.javascript_path).to eq("vindicia.js")
+    end
+
+    it "it throws an error for a non configurable attribute" do
+      expect {
+        Cashbox::Hoa.config do |config|
+          config.cannot_configure = "sekret"
+        end
+      }.to raise_error(NoMethodError)
+    end
+  end
 end
