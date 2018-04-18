6b6ed642032284e8736b3fa7f0adbd05d8be773d
Maulin Pathare <mpathare@gmail.com>
2018-04-18 16:46:45 -0400

    Allow Cashbox::Hoa to be configured

--- spec/cashbox/hoa/web_spec.rb
+++ spec/cashbox/hoa/web_spec.rb
@@ -1,9 +1,32 @@
 require "spec_helper"
 
 RSpec.describe "Cashbox::Hoa::Web" do
-
   def app
     Cashbox::Hoa::Web.new
+  end
+
+  before do
+    Cashbox::Hoa.config do |c|
+      c.private_key = "key"
+      c.auth_id = "auth"
+      c.javascript_path = "vindicia.js"
+    end
+  end
+
+  describe "the default layout" do
+    it "adds a script tag for the vindicia js" do
+      get "/payment_methods/credit_card/new"
+
+      expect(last_response.body).to include("<script src='vindicia.js'></script>")
+    end
+
+    it "shows a message when the js path is not configured" do
+      Cashbox::Hoa.javascript_path = nil
+
+      get "/payment_methods/credit_card/new"
+
+      expect(last_response.body).to include("Please configure the path for the Vindicia hoa javascript")
+    end
   end
 
   describe "requesting a valid credit card form" do
